b804eacd57f3e7503fd4862185f00355e25fde15
rajritik4041 <rajritik.9236@gmail.com>
2025-11-11 15:13:06 +0530

    connect cloud mongo but solve a login setup 2

--- breed-sense-app/src/app/api/users/login/route.js
+++ breed-sense-app/src/app/api/users/login/route.js
@@ -18,7 +18,7 @@ export async function POST(request) {
             console.log("I am hear 3")
             return NextResponse.json({ error: "User does not exist" }, { status: 400 });
         }
-        
+
         // check password
         console.log("I am hear 4")
         const validPassword = await bcryptjs.compare(password, user.password);
@@ -27,7 +27,7 @@ export async function POST(request) {
             console.log("I am hear 6")
             return NextResponse.json({ error: "Invalid Password" }, { status: 400 });
         }
-        
+
         // create token data
         console.log("I am hear 7")
         const tokenData = {
@@ -36,7 +36,9 @@ export async function POST(request) {
             email: user.email,
         };
         console.log("I am hear 8")
-        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" });
+        // const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" });
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET || "fallbackSecret", { expiresIn: "1d" }
+        );
 
         const response = NextResponse.json({
             message: "Login Successful",
